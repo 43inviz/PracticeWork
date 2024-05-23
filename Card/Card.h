3ca5872ec3839675f651abf701ef1eb0cfcc12e8
Max Teplov <153938230+43inviz@users.noreply.github.com>
2024-05-23 16:44:43 +0300

    123

--- Card/Card.h
+++ Card/Card.h
@@ -11,23 +11,15 @@ using namespace std;
 class Card :public Payment {
 	int _type;
 public:
-<<<<<<< HEAD
+
 	Card() = default;
-	Card(string name,string login,string pass,int type):Payment(name,login,pass),_type(type){
+	Card(string name, string login, string pass, int type) :Payment(name, login, pass), _type(type) {
 		vector<int> cardNumber(10);
 		for (auto i = cardNumber.begin(); i != cardNumber.end(); i++) {
 			*i = rand() % 9;
 		}
 
 		this->setNumber(cardNumber);
-=======
-	Card() {};
-	Card(string name, string login, string pass, int type) :User(name, login, pass), _type(type),_currentBalance(0) {
-			
-		for (int i = 0; i < 10; i++) {
-			_cardNumber.push_back(rand() % 9);
-		}
->>>>>>> 3d1b0ac31f0de87e69218065688c5b99e9717857
 	}
 
 	void setType(int type) { _type = type; }
